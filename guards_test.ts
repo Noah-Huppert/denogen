c460439e9aec5c5c64a542d00c76aa39d23f2d50
Noah Huppert <contact@noahh.io>
2020-10-31 13:28:42 -0400

    added isNumber sub type guard

---
+++ guards_test.ts
@@ -0,0 +1,17 @@
+import {
+  assertEquals,
+} from 'https://deno.land/std@0.75.0/testing/asserts.ts';
+
+import {
+    isNumber
+} from './guards.ts';
+
+Deno.test('isNumber()', () => {
+    let yesNum: number = 1;
+    let noNum: boolean = false;
+
+    assertEquals(isNumber(yesNum), true,
+			  'Expected isNumber on number to be true');
+    assertEquals(isNumber(noNum), false,
+			  'Expected isNumber on boolean to be false');
+});
