eeb3bc58e276e111b5301a2b21af2babb43dcf69
Zach Osman <zosman@gmu.edu>
2020-11-09 15:17:14 -0500

    initial typeguard creation very basic at the moment

--- guards.ts
+++ guards.ts
@@ -1,3 +1,17 @@
 export function isNumber(v: unknown): v is number {
   return typeof (v) === "number";
 }
+
+export function isBoolean(v: unknown): v is boolean {
+    return typeof(v) === 'boolean';
+}
+
+export function isString(v: unknown): v is string {
+    return typeof(v) === 'string';
+}
+
+export function isStringMatch(v: string, w: string) {
+    isString(v);
+    isString(w);
+    return v === w;
+}
